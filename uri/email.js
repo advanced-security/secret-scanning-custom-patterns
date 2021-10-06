52b2f2f0c02b1f37c9201e354a8e70405973ada6
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2021-10-06 13:17:28 +0000

    Add email URIs

--- uri/email.js
+++ uri/email.js
@@ -4,5 +4,6 @@ const maintainerEmail = "email@gmail.com";
 const securityEmailAddress = "security@example.com";
 const accountingEmailAddress = "accounting@example.com";
 
-
-
+const uri = "https://example.com"
+const uriUsername = "https://username@example.com"
+const uriUsernamePath = "https://username@example.com/path"
