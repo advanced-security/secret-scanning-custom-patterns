ac9b68b6b7e6014dae50e8e301d079de9f6b4aa1
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2022-04-28 10:30:32 +0000

    Add more cases

--- configs/django/settings.py
+++ configs/django/settings.py
@@ -2,4 +2,9 @@ SECRET_KEY = "DJango_Secret_Key1"
 SECRET_KEY = "DJango_Secret_Key2"
 SECRET_KEY = "DJango_Secret_Key3"
 
+# False positives
+
+SECRET_KEY = os.environ.get("test")
+SECRET_KEY = os.environ["test"]
+
 secret_key = "Django_ThisIsAFalsePositive"
