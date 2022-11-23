09e450099b3eed627e59abc77b6146177e85ea84
aegilops <41705651+aegilops@users.noreply.github.com>
2022-11-23 15:00:28 +0000

    Updated to use start-of-match flag where possible. Getting there

--- testing/test.py
+++ testing/test.py
@@ -17,6 +17,7 @@ import hyperscan
 from functools import partial
 from dataclasses import dataclass
 import re
+from typing import Union
 
 
 LOG = logging.getLogger(__name__)
@@ -37,7 +38,7 @@ class Pattern():
 
     def regex_string(self) -> bytes:
         """Concatenate and UTF-8 encode."""
-        return f"(?:{self.start})({self.pattern})(?:{self.end})".encode('utf-8')
+        return f"({self.start})({self.pattern})({self.end})".encode('utf-8')
 
 
 def parse_patterns(patterns_dir: str):
@@ -62,6 +63,28 @@ def parse_patterns(patterns_dir: str):
     return patterns
 
 
+def compile(db: hyperscan.Database, regex: Union[str|list[str]]) -> bool:
+    """Compile one or more hyperscan regexes into the given database."""
+    if isinstance(regex, str):
+        regex_bytes = [regex.encode('utf-8')]
+    else:
+        regex_bytes = [r.encode('utf-8') for r in regex]
+    try:
+        db.compile(regex_bytes, flags=hyperscan.HS_FLAG_SOM_LEFTMOST)
+    except hyperscan.error as err:
+        try:
+            db.compile(regex_bytes)
+        except hyperscan.error as err:
+            LOG.error("Failed to compile rules")
+            for r in regex_bytes:
+                try:
+                    db.compile(r)
+                except hyperscan.error as err:
+                    LOG.error("Hyperscan error with '%s': %s", r, err)
+            return False
+    return True
+
+
 def report_scan_results(patterns, content, rule_id, start_offset, end_offset, flags, context):
     match_content = content[start_offset:end_offset]
     pattern = patterns[rule_id]
@@ -75,8 +98,11 @@ def report_scan_results(patterns, content, rule_id, start_offset, end_offset, fl
     # now do the start/pattern/end matches in sequence to understand the boundaries of the pattern match
     # it's a callback chain, from each db.scan(..., callback) call
     db_start = hyperscan.Database()
-    db_start.compile([pattern.start.encode('utf-8')])  # TODO: add anchors?
-    db_start.scan(match_content, partial(handle_pattern_start, pattern, match_content))
+    if compile(db_start, pattern.start):
+        db_start.scan(match_content, partial(handle_pattern_start, pattern, match_content))
+    else:
+        LOG.error('Error with start regex: %s', regex) 
+
 
 
 def handle_pattern_start(pattern, content, rule_id, start_offset, end_offset, flags, context):
@@ -86,52 +112,75 @@ def handle_pattern_start(pattern, content, rule_id, start_offset, end_offset, fl
     LOG.debug("Matched start: %s", match_content)
 
     if start_offset != 0:
-        raise ValueError("Start offset is not zero: %d", start_offset)
+        LOG.warning("Start offset is not zero: {:d}".format(start_offset))
+        return
 
     remaining_content = content[end_offset:]
     
-    db_end = hyperscan.Database()
-    db_end.compile([b'(' + pattern.end.encode('utf-8') + b')$'])  # TODO: add anchors?
-    db_end.scan(remaining_content, partial(handle_pattern_end, pattern, remaining_content))
-
-
-def handle_pattern_end(pattern, content, rule_id, start_offset, end_offset, flags, context):
-    match_content = content[start_offset:end_offset]
-    
-    LOG.debug("Matched id %s at %d:%d with flags %s and context %s", rule_id, start_offset, end_offset, flags, context)
-    LOG.debug("Matched end: %s", match_content)
-
-    if end_offset != len(content):
-        raise ValueError("End offset is not the end of the content: %d != %d", end_offset, len(content))
-
-    remaining_content = content[:start_offset]
-
-    LOG.debug("Remaining content to match pattern on: %s", remaining_content)
-
+    # pass on to the pattern match now
     db_pattern = hyperscan.Database()
-    db_pattern.compile([pattern.pattern.encode('utf-8')])  # TODO: add anchors?
-    db_pattern.scan(remaining_content, partial(handle_pattern_match, pattern, remaining_content))
+    if compile(db_pattern, pattern.pattern):
+        db_pattern.scan(remaining_content, partial(handle_pattern_match, pattern, remaining_content))
+    else:
+        LOG.error('Error with pattern regex: %s', pattern.pattern) 
 
 
 def handle_pattern_match(pattern, content, rule_id, start_offset, end_offset, flags, context):
     match_content = content[start_offset:end_offset]
     
     LOG.debug("Matched id %s at %d:%d with flags %s and context %s", rule_id, start_offset, end_offset, flags, context)
-    LOG.info("Matched pattern: %s", match_content)
-    
+ 
     if start_offset != 0:
-        raise ValueError("Start offset is not zero: %d", start_offset)
+        LOG.warning("Start offset is not zero: {:d}".format(start_offset))
+        return
 
-    if end_offset != len(content):
-        raise ValueError("End offset is not the end of the content: %s", end_offset)
+    LOG.debug("Matched pattern: %s", match_content)
+    
+    remaining_content = content[end_offset:]
+    
+    db_end = hyperscan.Database()
 
+    if len(remaining_content) == 0:
+        LOG.info("Total match!")
+        return
 
+    if compile(db_end, pattern.end):
+        db_end.scan(remaining_content, partial(handle_pattern_end, pattern, remaining_content))
+    else:
+        LOG.error('Error with end regex: %s', regex) 
+
+ 
+def handle_pattern_end(pattern, content, rule_id, start_offset, end_offset, flags, context):
+    match_content = content[start_offset:end_offset]
+    
+    LOG.debug("Matched id %s at %d:%d with flags %s and context %s", rule_id, start_offset, end_offset, flags, context)
+    LOG.debug("Matched end: %s", match_content)
+
+    if flags & hyperscan.HS_FLAG_SOM_LEFTMOST:
+        if start_offset != 0:
+            LOG.warning("Start offset is not zero: {:d}".format(start_offset))
+            return
+    else:
+        LOG.debug("This pattern may be over-matching, it does not report a start offset")
+
+    LOG.info("Total match!")
+  
+  
 def test_patterns(tests_path: str, db: hyperscan.Database):
     for dirpath, dirnames, filenames in os.walk(tests_path):
         if PATTERNS_FILENAME in filenames:
             LOG.debug("Found patterns in %s", dirpath)
             patterns = parse_patterns(dirpath)
-            db.compile([pattern.regex_string() for pattern in patterns])
+            try:
+                db.compile([pattern.regex_string() for pattern in patterns], flags=hyperscan.HS_FLAG_SOM_LEFTMOST)
+            except hyperscan.error as err:
+                LOG.error("Hyperscan error: %s", err)
+                for pattern in patterns:
+                    try:
+                        db.compile([pattern.regex_string()], flags=hyperscan.HS_FLAG_SOM_LEFTMOST)
+                    except hyperscan.error as err:
+                        LOG.error("Pattern error: %s", pattern.regex_string())
+                exit
             for filename in filenames:
                 with open(os.path.join(dirpath, filename), "rb") as f:
                     content = f.read()
